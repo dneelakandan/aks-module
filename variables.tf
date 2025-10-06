ed6286e58bc88d0e26334d84eab954f929965190
dneelakandan <40385949+dneelakandan@users.noreply.github.com>
2025-10-06 10:59:20 +0530

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,36 @@
 
 variable "cluster_name" {
-  type = string
+  type        = string
+  description = "Name of the AKS cluster"
+  default     = "lbs-aks-cluster"
 }
 
 variable "location" {
-  type = string
+   type        = string
+  description = "Azure region for AKS deployment"
+  default     = "West Europe"
 }
 
 variable "aks_resource_group" {
-  type = string
+  type        = string
+  description = "Name of the resource group"
+  default     = "lbs-rg"
 }
 
 variable "node_count" {
-  type = number
+  type        = number
+  description = "Number of nodes in the default node pool"
+  default     = 2
 }
 
 variable "node_vm_size" {
-  type = string
+  type        = number
+  description = "Number of nodes in the default node pool"
+  default     = 2
 }
 
 variable "vnet_subnet_id" {
-  type = string
+  type        = string
+  description = "Subnet ID for AKS node pool"
+  default     = "/subscriptions/your-subscription-id/resourceGroups/lbs-rg/providers/Microsoft.Network/virtualNetworks/lbs-aks-vnet/subnets/lbs-aks-vnet-subnet"
 }
